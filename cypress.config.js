984ad8a6cdb994ef7b9d06a387c6761ddd0fabf1
Jewel Dacion <dacionjewelshayne01@gmail.com>
2023-01-27 08:27:03 -0500

    refractor

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://qa.delekhomes.com'
+    baseUrl: 'https://f4hatlr72b.execute-api.us-east-1.amazonaws.com/production'
   }
 })
