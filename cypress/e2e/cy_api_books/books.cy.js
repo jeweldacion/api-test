985d05951fbe84e3d2af7bf8842de7a08ca22fb6
Jewel Dacion <dacionjewelshayne01@gmail.com>
2023-01-29 21:37:24 -0500

    three new test cases added

--- cypress/e2e/cy_api_books/books.cy.js
+++ cypress/e2e/cy_api_books/books.cy.js
@@ -1,17 +1,18 @@
 import { faker } from "@faker-js/faker";
 
-const baseUrl = "https://f4hatlr72b.execute-api.us-east-1.amazonaws.com/production";
+//const baseUrl = "https://f4hatlr72b.execute-api.us-east-1.amazonaws.com/production";
 const randomBookId = faker.datatype.uuid();
 const randomAuthor = faker.name.fullName();
 const randomTitle = faker.random.words();
+const newRandomTitle = faker.random.words();
 let responseBookId, responseAuthor, responseTitle;
 
 describe("Books", () => {
   beforeEach(() => {
-    cy.request("POST", baseUrl + "/books", {
+    cy.request("POST", "/" + "/books", {
       title: randomTitle,
       author: randomAuthor,
-      id: randomBookId
+      id: randomBookId,
     }).then((response) => {
       responseBookId = response.body.id;
       responseAuthor = response.body.author;
@@ -21,17 +22,48 @@ describe("Books", () => {
   });
 
   it("Shoould create a book", () => {
-    expect(responseTitle).to.equal(randomTitle);
-    expect(responseAuthor).to.equal(randomAuthor);
-    expect(responseBookId.length).to.equal(32);
- });
+      expect(responseTitle).to.equal(randomTitle);
+      expect(responseAuthor).to.equal(randomAuthor);
+      expect(responseBookId.length).to.equal(32);
+  });
 
   it("Should get a books", () => {
-    cy.request(baseUrl + "/" + responseBookId).then((response) => {
+    cy.request("/" + `/${responseBookId}`).then((response) => {
       expect(response.status).to.eq(200);
       expect(responseTitle).to.equal(randomTitle);
       expect(responseAuthor).to.equal(randomAuthor);
       expect(responseBookId.length).to.equal(32);
+    });
+  });
+
+  it("Should get all books", () => {
+    cy.request("GET", "/" + "/books").then((response) => {
+      expect(response.status).to.eq(200);
+
+      response.body.body.forEach((book) => {
+        expect(book.id.length).to.equal(32);
+        expect(book.title.length).to.be.gt(0);
+        expect(book.author.length).to.be.gt(0);
+      });
+    });
+  });
+
+  it("Should update a book", () => {
+    cy.request("PUT", "/" + `${responseBookId}`, {
+      title: newRandomTitle,
+      author: randomAuthor,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.title).to.equal(newRandomTitle);
+      expect(responseAuthor).to.equal(randomAuthor);
+      expect(responseBookId.length).to.equal(32);
+    });
+  });
+
+  it("Should delete a book", () => {
+    cy.request("DELETE", "/" + `${responseBookId}`).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.message).to.equal("Book was removed successfully");
     });
   });
 });
